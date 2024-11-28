895ec76c740920b5228aac70f83f9d791af38630
Joskemu <vadhelkemu098@gmail.com>
2024-11-28 21:31:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-//your JS code here. If required.
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+	event.preventDefault();
+
+	const username = document.getElementById('username').value;
+    	const password = document.getElementById('password').value;
+	const chcek  = document.getElementById('checkbox').checked;
+
+	if(Remember me){
+		localStorage.setItem('username',username);
+		localStorage.setItem('password',password);
+	}else{
+		localStorage.removeItem('username',username);
+		localStorage.removeItem('password',password);
+	}
+	alert('Logged in as ${username}');
+
+	checkExistingUser();
+});
+
+function checkExistingUser()
+	{ 
+		const username = localStorage.getItem('username');
+		const password = localStorage.getItem('password'); 
+		const existingButton = document.getElementById('existing');
+	if (username && password)
+	{ 
+		existingButton.style.display = 'block'; 
+		existingButton.onclick = function()
+			{ 
+				alert(`Logged in as ${username}`);
+			}
+	} else {
+		existingButton.style.display = 'none';
+	} 
+	
+	}
+	checkExistingUser();
